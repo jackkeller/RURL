928e45806de05a7491b7918820a4deb7d766ee4e
Jack Keller <jack.keller@wwt.com>
2022-01-14 14:00:00 -0600

    Got tests working, this was a bit tricky in vue3

---
+++ src/views/Home.test.js
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/vue'
+import Home from './Home.vue'
+
+describe('Home', () => {
+
+  it('should load the home view with the header Home Page', () => {
+    const { getByText } = render(Home)
+    const Header = getByText('Home Page')
+
+    expect(Header).toBeInTheDocument()
+  })
+
+  it('should not be a flying spaghetti monster', () => {
+    const { queryByText } = render(Home)
+    const Monster = queryByText('Flying Spaghetti Monster')
+
+    expect(Monster).toBeFalsy() // The monster can not hurt you
+  })
+});
