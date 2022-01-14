38fe7f83eb337e6236e4f9f5047bf46b24ffb1ca
Jack Keller <jack.keller@wwt.com>
2022-01-14 09:53:00 -0600

    Adds routing, sass, and some basic layout styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="layout">
+    <header>
+      <div class="container">
+        <div class="logo">{{ title }}</div>
+        <nav id="nav">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <router-view />
+      </div>
+    </main>
+    <footer>
+      <div class="container">
+        <small>&copy; {{ year }} {{ title }}</small>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      title: 'RURL',
+      year: new Date().getFullYear()
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  @import "@/styles/app.sass"
+
+  .logo
+    font-size: 1.4rem 
+
+  nav
+    a
+      color: var(--nav-link-color)
+      font-weight: bold
+      text-transform: uppercase
+      text-decoration: none
+      padding: 0 .5rem
+
+      &:not(:last-child)
+        margin-right: 1rem
+
+      &.router-link-exact-active
+        color: var(--nav-link-active-color)
+        border-bottom: 4px solid var(--text-secondary)
 </style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'RURL'
+    }
+  },
+  css: {
+    loaderOptions: {
+      sass: {}
+    }
+  }
+}
