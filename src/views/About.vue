2724e69337717496a8cbc1048f86f7a56bf9658a
Jack Keller <jack.keller@wwt.com>
2022-01-15 13:40:31 -0600

    Updates about page, this should serve as human readable note/thoughts along the way

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>About Page</h1>
-  </div>
+  <section class="about">
+    <h1>A Learning Experience</h1>
+    <p>Never have I ever ... worked on a Vue.js project. Overall I like working with it however some mistakes were
+      made along the way. I found my preferred tooling for testing was not well supported</p>
+  </section>
 </template>
