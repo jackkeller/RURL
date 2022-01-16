b596f6d4324d2edb5795d10f2ff42a630bf6dae3
Jack Keller <jack.keller@wwt.com>
2022-01-16 15:40:27 -0600

    Filtering drinks by title

--- src/components/Drinks.vue
+++ src/components/Drinks.vue
@@ -9,20 +9,35 @@
       </select>
       <h1>{{ selectedDrinksType }}</h1>
     </div>    
+    <input type="text" placeholder="Filter Drinks..." name="search" @keyup="filterDrinks($event.target.value || null)" ref="search">
   </section>
   
   <section v-if="!selectedDrinks.length">
     <h2>Loading...</h2>
   </section>
-
-  <section v-if="selectedDrinks.length">
+  
+  <section v-if="filteredToNothing">
+    <h2>No drinks found...</h2>
+  </section>
+  
+  <section v-if="selectedDrinks.length && !filteredDrinks.length && !filteredToNothing">
     <div class="drinks">
       <div class="drink" v-for="drink in selectedDrinks" :key="drink.idDrink">
-        <h3>{{ drink.strDrink }}</h3>
-        <img :src="drink.strDrinkThumb" :alt="drink.strDrink" :title="drink.strDrink">
+        <strong class="drink-heading">{{ drink.strDrink }}</strong>
+        <img :src="drink.strDrinkThumb" :alt="drink.strDrink" :title="drink.strDrink" height="100" width="100">
       </div>
     </div>
   </section>
+
+  <section v-if="filteredDrinks.length">
+    <div class="drinks">
+      <div class="drink" v-for="drink in filteredDrinks" :key="drink.idDrink">
+        <strong class="drink-heading">{{ drink.strDrink }}</strong>
+        <img :src="drink.strDrinkThumb" :alt="drink.strDrink" :title="drink.strDrink" height="100" width="100">
+      </div>
+    </div>
+  </section>
+
 </template>
 
 <script>
@@ -39,6 +54,8 @@ export default {
     return {
       categoryList: [],
       selectedDrinks: [],
+      filteredDrinks: [],
+      filteredToNothing: false,
       selectedDrinksType: '',
     }
   },
@@ -55,12 +72,35 @@ export default {
       fetch(api.filter + category)
         .then(res => res.json())
         .then(data => {
+          this.filteredDrinks = []
+          this.$refs.search.value = ''
           this.selectedDrinks = data.drinks
         })
+    },
+    filterDrinks(filter) {
+      let filteredDrinks = this.selectedDrinks
+      if (filter) {
+        filteredDrinks = filteredDrinks.filter(drink => {
+          return drink.strDrink.toLowerCase().includes(filter.toLowerCase())
+        })
+      
+        if (filteredDrinks.length) {
+          this.filteredToNothing = false
+          this.filteredDrinks = filteredDrinks 
+        } else {
+          this.filteredToNothing = true
+        }
+
+        this.filteredDrinks = filteredDrinks
+        return filteredDrinks
+      } else {
+        this.filteredDrinks = []
+      }
     }
   },
   mounted() {
-    this.getDrinkCategories(),
+    this.getDrinkCategories()
+
     setTimeout(() => {
       this.filterCategory(this.categoryList[0].strCategory)
     }, 1000)
